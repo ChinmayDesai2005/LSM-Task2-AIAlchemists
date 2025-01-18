85576d805f0e083141e883f6e35fd4e36519c4e3
Gautam-04 <2022.gautam.rai@ves.ac.in>
2025-01-18 23:11:37 +0530

    made pages
    Signed-off-by: Gautam-04 <2022.gautam.rai@ves.ac.in>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes,Route } from "react-router-dom"
 import Home from "./pages/home/Home"
 import Input from "./pages/input/Input"
+import Header from "./components/header/Header"
 function App() {
 
   return (
     <>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Input" element={<Input />} />
--- frontend/src/pages/input/Input.jsx
+++ frontend/src/pages/input/Input.jsx
@@ -1,8 +1,29 @@
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import './Input.css';
 
-function Input() {
+function InputPage() {
+  const location = useLocation();
+  const { text, file } = location.state || {};
+  const [inputValue, setInputValue] = useState(text || (file && file.name) || "");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
-    <div>Input</div>
-  )
+    <div className="inputPage">
+      <h1>Input Data</h1>
+      <div className="inputField">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Your text or file name here"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Input
+export default InputPage;
