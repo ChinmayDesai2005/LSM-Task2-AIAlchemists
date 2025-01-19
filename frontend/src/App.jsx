e38f5a1db79135c4bd93f52b4bcd2d937bafdfff
Chinmay Desai <chinmaydesai2005@gmail.com>
2025-01-19 06:40:06 +0530

    added newer translate model

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,28 @@
-import { Routes,Route } from "react-router-dom"
-import Home from "./pages/home/Home"
-import Input from "./pages/input/Input"
-import Header from "./components/header/Header"
-import Profile from "./pages/profile/Profile"
-import BlogsPage from "./pages/blog_coverpage/BlogsPage"
-import Transcript from "./pages/transcript/Transcript"
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Input from "./pages/input/Input";
+import Header from "./components/header/Header";
+import Profile from "./pages/profile/Profile";
+import BlogsPage from "./pages/blog_coverpage/BlogsPage";
+import Transcript from "./pages/transcript/Transcript";
 
-import Auth from "./pages/auth/Auth"
+import Auth from "./pages/auth/Auth";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-
   return (
     <>
-    <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/Input" element={<Input />} />
-        <Route path="/Profile" element={<Profile/>} />
-        <Route path="/Blog" element={<BlogsPage/>} />
-        <Route path="/Transcript" element={<Transcript/>} />
+        <Route path="/input" element={<Input />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/blog" element={<BlogsPage />} />
+        <Route path="/transcript" element={<Transcript />} />
       </Routes>
-        <ToastContainer
+      <ToastContainer
         position="bottom-center"
         autoClose={5000}
         hideProgressBar={false}
@@ -36,7 +35,7 @@ function App() {
         theme="colored"
       />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
